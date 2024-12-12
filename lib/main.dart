57bd529b06f7234cc7019629a4b213a52acbec9a
Prof. Luiz Camilo <luiz.camilo4@etec.sp.gov.br>
2024-12-12 17:20:29 -0300

    full project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:camera_flutter/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ML Kit',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
